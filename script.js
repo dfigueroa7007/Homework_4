6a816388c1c48d65c765e0a02658f55d5ee5dfd5
Daniel Figueroa <dfigueroa7007@gmail.com>
2020-10-11 22:17:07 -0700

    Added questions and numbers array into JS file.

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+function countdownTimer(params) {
+    
+}
+var counter = 0;
+
+var countdownArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+var questions = [
+    {
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        answers: [
+            {text: "(A) <script>", correct: true },
+            {text: "(B) <JavaScript>", correct: false}
+            {text: "(C) <JScript>", correct: false}
+            {text: "(D) <scripts>", correct: false}
+        ]
+        question: "What is CSS short for?",
+        answers: [
+            {text: "(A) Case Sensitive Style", correct: true },
+            {text: "(B) Case Sensitive Styling"}, correct: false },
+            {text: "(C) Content Styling Sheet"}, correct: false },
+            {text: "(D) Cascading Style Sheets"}, correct: false },
+        ]
+        question: "What is DOM short for?",
+        answers: [
+            {text: "(A) Document Object Model", correct: true },
+            {text: "(B) Document Obtained Model"}, correct: false },
+            {text: "(C) Domain Object Model"}, correct: false },
+            {text: "(D) Detailed Object Markup"}, correct: false },
+        ]
+        question: "When == is used in an IF STATEMENT, which of the below is true?",
+        answers: [
+            {text: "(A) The items are not equal", correct: true },
+            {text: "(B) Both value and type are equal"}, correct: false },
+            {text: "(C) It is a true statement"}, correct: false },
+            {text: "(D) The values are equal"}, correct: true },
+        ]
+        question: "What is the syntax of the function that prints a message to the debugging console?",
+        answers: [
+            {text: "(A) console.logs()", correct: true },
+            {text: "(B) consoles.log()"}, correct: false },
+            {text: "(C) console.log()"}, correct: true },
+            {text: "(D) idkmybffJill.log()"}, correct: false },
+        ]
+    }
+]
